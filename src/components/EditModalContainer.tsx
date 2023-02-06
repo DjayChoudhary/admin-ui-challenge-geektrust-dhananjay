7bb80fca7a1dcf88e2c2e7536e9a50e6071a3014
Dhananjay Choudhary <40604207+DjayChoudhary@users.noreply.github.com>
2023-02-06 23:22:44 +0530

    create Edit Modal Container component

---
+++ src/components/EditModalContainer.tsx
@@ -0,0 +1,48 @@
+interface IRowMemberData {
+  selected?: boolean;
+  name: string;
+  email: string;
+  role: string;
+  id: string;
+}
+export default function EditModalContainer({
+  modalRef,
+  memberData,
+}: {
+  modalRef: React.MutableRefObject<null | HTMLDivElement>;
+  memberData?: IRowMemberData;
+}) {
+  return (
+    <div
+      ref={modalRef}
+      id="editModal"
+      className="fixed top-0 left-0 right-0 z-50 hidden w-full mt-8 p-4 overflow-x-hidden overflow-y-auto md:inset-0"
+    >
+      <div className="relative w-full max-w-2xl md:auto bg-white shadow-lg p-4 mx-auto">
+        <div className="relative text-xl text-gray-700 font-bold bg-white rounded-lg shadow p-2 dark:bg-gray-700">
+          Edit Member Data
+        </div>
+        <div className="flex flex-col gap-4">
+          <span>Id: {memberData?.id}</span>
+          <span>name: {memberData?.name}</span>
+          <span>email: {memberData?.email}</span>
+          <span>role: {memberData?.role}</span>
+        </div>
+
+        <div className="flex justify-start gap-6">
+          <button className="px-2 py-1 text-white font-semibold bg-blue-600 rounded-md">
+            Save
+          </button>
+          <button
+            onClick={() => {
+              modalRef.current!.style.display = "none";
+            }}
+            className="px-2 py-1 font-semibold text-gray-600 rounded-md bg-white border-2 border-gray-200"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
